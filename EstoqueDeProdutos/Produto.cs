2b20f305871f8f8225e11d5682f843f7b1e90f28
KaioDD <kkaiodantas6@gmail.com>
2023-07-14 08:36:00 -0300

    Adicionar arquivos de projeto.

---
+++ EstoqueDeProdutos/Produto.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace EstoqueDeProdutos
+{
+    public class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public double CalcularvalorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+        public void AdicionarProdutos(int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+        public void RemoverProduto(int quantidade) 
+        {
+            Quantidade -= quantidade;
+        }
+        private string Apresentar()
+        {
+            string apresentar = $"{Nome}, \nPreço: {Preco}, \nQuantidade: {Quantidade}, \nTotal: R${CalcularvalorTotalEmEstoque()}";
+            return apresentar;
+        }
+
+        public override string ToString()
+         //Reescrita do metodo toString para apresentar o produto de forma "Personalizada"
+        {
+            return Apresentar();    
+        }
+    }
+}
---
+++ EstoqueDeProdutos/Program.cs
@@ -0,0 +1,45 @@
+﻿using EstoqueDeProdutos;
+using System.Xml;
+using System.Globalization;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Produto p = new Produto();
+
+
+        Console.WriteLine("Bem Vindo ao gerenciador de estoque");
+        Console.WriteLine("Entre com os dados do produto");
+        Console.Write("Nome: ");
+        p.Nome = Console.ReadLine();
+
+        Console.Write("Preço: ");
+        p.Preco = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+        Console.Write("Quantidade disponivel em estoque: ");
+        p.Quantidade = int.Parse(Console.ReadLine());
+
+        Console.WriteLine();
+        Console.WriteLine("Dados do produto: " + p);
+        // o P vai ser representado pelo metodo Tostring que foi reescrito na classe produto 
+
+        Console.WriteLine();
+        Console.Write("Digite a quantidade de produtos a ser adicionado no estoque: ");
+        int qte = int.Parse(Console.ReadLine());
+        p.AdicionarProdutos(qte);
+        // O metodo adicionar produtos vai receber o valor inserido na variavel "qte" e somar ao valor ja existente na quantidade inicial.
+        Console.WriteLine("");
+        Console.WriteLine($"Dados atualizado: {p}");
+
+        Console.Write("Digite a quantidade de produtos a ser removido do estoque:  ");
+        qte = int.Parse(Console.ReadLine());
+        p.RemoverProduto(qte);
+
+        Console.WriteLine("");
+        Console.WriteLine($"Dados atualizado: {p}");
+
+
+    }
+
+}
